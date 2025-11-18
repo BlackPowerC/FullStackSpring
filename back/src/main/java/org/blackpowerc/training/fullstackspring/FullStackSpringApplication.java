6abc28deeb7ccb25d101c78f5ebf8bd607577cb6
jojo <jordy.fatigba@theopentrade.com>
2025-11-18 14:27:27 +0100

    Initial commit.

---
+++ back/src/main/java/org/blackpowerc/training/fullstackspring/FullStackSpringApplication.java
@@ -0,0 +1,13 @@
+package org.blackpowerc.training.fullstackspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullStackSpringApplication.class, args);
+    }
+
+}
---
+++ back/src/test/java/org/blackpowerc/training/fullstackspring/FullStackSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.blackpowerc.training.fullstackspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
