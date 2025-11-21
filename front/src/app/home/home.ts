b08a67773b50532489486617a840a154296c44f5
jojo <jordy.fatigba@theopentrade.com>
2025-11-21 14:25:21 +0100

    Display locations in home component.

--- front/src/app/home/home.ts
+++ front/src/app/home/home.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HousingService } from "../housing.service";
+import type {HousingLocationInfo} from "../housing-location";
+import {HousingLocation} from "../housing-location/housing-location";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HousingLocation],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrls: ['./home.css'],
+  standalone: true
 })
 
-export class Home {}
+export class Home
+{
+  housingService: HousingService = inject(HousingService) ;
+  housingLocationList: HousingLocationInfo[] ;
+
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocation() ;
+  }
+
+}
--- front/src/app/housing-location/housing-location.ts
+++ front/src/app/housing-location/housing-location.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, input} from '@angular/core';
+import type {HousingLocationInfo} from "../housing-location";
 
 @Component({
   selector: 'app-housing-location',
   imports: [],
   templateUrl: './housing-location.html',
   styleUrl: './housing-location.css',
+  standalone: true
 })
-export class HousingLocation {
 
+export class HousingLocation {
+  housingLocation = input.required<HousingLocationInfo>() ;
 }
