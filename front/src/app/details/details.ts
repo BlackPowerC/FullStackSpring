46500bed359ab67e55a98c8a0895677342f4dd1b
jojo <jordy.fatigba@theopentrade.com>
2025-11-21 16:03:37 +0100

    Customize details component with html template and new css

--- front/src/app/details/details.ts
+++ front/src/app/details/details.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {HousingService} from "../housing.service";
+import {ActivatedRoute} from "@angular/router";
+import type {HousingLocationInfo} from "../housing-location";
 
 @Component({
   selector: 'app-details',
   imports: [],
   templateUrl: './details.html',
   styleUrl: './details.css',
+  standalone: true
 })
-export class Details {
+export class Details
+{
+  route: ActivatedRoute = inject(ActivatedRoute) ;
+  housingService: HousingService = inject(HousingService) ;
+  housingLocation?: HousingLocationInfo ;
+  housingLocationId: number = -1 ;
 
+  constructor() {
+    this.housingLocationId = Number(this.route.snapshot.params["id"]) ;
+    this.housingLocation = this.housingService.getHouseLocationById(this.housingLocationId) ;
+  }
 }
