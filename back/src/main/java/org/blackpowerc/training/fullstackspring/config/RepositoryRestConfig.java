8449a0653dcfa4bd2c0799e434d4f5e26e553536
jojo <jordy.fatigba@theopentrade.com>
2025-12-13 20:50:02 +0000

    feat(backend): configuration pour exposer les identifiants des entités.

---
+++ back/src/main/java/org/blackpowerc/training/fullstackspring/config/RepositoryRestConfig.java
@@ -0,0 +1,26 @@
+package org.blackpowerc.training.fullstackspring.config;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.Type;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class RepositoryRestConfig implements RepositoryRestConfigurer
+{
+    private final EntityManager em ;
+
+    @Autowired
+    public RepositoryRestConfig(EntityManager em) {
+        this.em = em ;
+    }
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors)
+    {
+        config.exposeIdsFor(em.getMetamodel().getEntities().stream().map(Type::getJavaType).toList().toArray(new Class[0]))  ;
+    }
+}
