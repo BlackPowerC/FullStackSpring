01f5f4c89a04a817932b2d42d499eac1b3fcf881
jojo <jordy.fatigba@theopentrade.com>
2025-12-14 15:20:41 +0000

    feat(frontend): configuration du client hateoas de angular.
    Url de base figé du localhost:8080.

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -1,14 +1,31 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import {CacheMode, provideNgxHateoasClient} from "@lagoshny/ngx-hateoas-client";
 
 import { routes } from './app.routes';
 import {provideProtractorTestingSupport} from "@angular/platform-browser";
+import {provideHttpClient} from "@angular/common/http";
+import {HousingLocationResource} from "./housing-location";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideProtractorTestingSupport(),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    provideNgxHateoasClient({
+      useTypes: {
+        resources: [HousingLocationResource]
+      },
+      http: {
+        rootUrl: "http://localhost:8080/"
+      },
+      cache: {
+        enabled: true,
+        mode: CacheMode.ALWAYS,
+        lifeTime: 3600 * 1000
+      }
+    })
   ]
 };
