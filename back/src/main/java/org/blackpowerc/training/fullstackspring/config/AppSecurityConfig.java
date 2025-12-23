eaaca3febd05315bc9d88709362a3052106e949b
jojo <jordy.fatigba@theopentrade.com>
2025-12-23 12:16:41 +0000

    feat(sécurité backend): création de 2 utilisateurs statiques client et admin en mémoire.

--- back/src/main/java/org/blackpowerc/training/fullstackspring/config/AppSecurityConfig.java
+++ back/src/main/java/org/blackpowerc/training/fullstackspring/config/AppSecurityConfig.java
@@ -2,14 +2,14 @@ package org.blackpowerc.training.fullstackspring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -31,10 +31,33 @@ public class AppSecurityConfig
     }
 
     @Bean
-    @Primary
     public UserDetailsService userManager()
     {
-        var defaultUser = new  User("jojo", "mon_super_mot_de_passe_1234", List.of(new SimpleGrantedAuthority("ROLE_ADMIN"))) ;
-        return new InMemoryUserDetailsManager(defaultUser) ;
+        var admin = User
+                .withUsername("jojo")
+                .disabled(false)
+                .accountExpired(false)
+                .accountLocked(false)
+                .passwordEncoder(passwordEncoder()::encode)
+                .password("mon_super_mot_de_passe_1234")
+                .roles("ADMIN")
+                .build() ;
+
+        var client = User
+                .withUsername("client")
+                .disabled(false)
+                .accountExpired(false)
+                .accountLocked(false)
+                .passwordEncoder(passwordEncoder()::encode)
+                .password("le_mot_de_pass_du_client_1234")
+                .roles("CLIENT")
+                .build() ;
+
+        return new InMemoryUserDetailsManager(admin, client) ;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder() ;
     }
 }
