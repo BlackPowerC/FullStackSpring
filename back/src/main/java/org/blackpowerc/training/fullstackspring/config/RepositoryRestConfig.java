61359f0a39de8f72bdfcc4e58df82d9e26a63500
jojo <jordy.fatigba@theopentrade.com>
2025-12-23 12:18:39 +0000

    feat( backend): ajout de bean validation pour la validation des entités.

--- back/src/main/java/org/blackpowerc/training/fullstackspring/config/RepositoryRestConfig.java
+++ back/src/main/java/org/blackpowerc/training/fullstackspring/config/RepositoryRestConfig.java
@@ -3,9 +3,11 @@ package org.blackpowerc.training.fullstackspring.config;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.metamodel.Type;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
@@ -19,8 +21,12 @@ public class RepositoryRestConfig implements RepositoryRestConfigurer
     }
 
     @Override
-    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors)
-    {
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         config.exposeIdsFor(em.getMetamodel().getEntities().stream().map(Type::getJavaType).toList().toArray(new Class[0]))  ;
+    }
+
+    @Bean
+    public MethodValidationPostProcessor methodValidationPostProcessor() {
+        return new MethodValidationPostProcessor() ;
     }
 }
