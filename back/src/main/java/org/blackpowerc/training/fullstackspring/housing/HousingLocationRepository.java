2f7d6c6237423e4aec44e4b285462b3ca5c8aa11
jojo <jordy.fatigba@theopentrade.com>
2025-12-24 11:43:32 +0000

    chore(backend): recherche des locations par ville ou nom sans la casse.

--- back/src/main/java/org/blackpowerc/training/fullstackspring/housing/HousingLocationRepository.java
+++ back/src/main/java/org/blackpowerc/training/fullstackspring/housing/HousingLocationRepository.java
@@ -1,9 +1,13 @@
 package org.blackpowerc.training.fullstackspring.housing;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Repository;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,18 +19,17 @@ import java.util.UUID;
  *
  * @author jordy fatigba72@gmail.com
  */
+@Validated
 @Repository
 @CrossOrigin(methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT})
 @RepositoryRestResource(collectionResourceRel = "housing_locations", path = "housing_locations")
 public interface HousingLocationRepository extends JpaRepository<HousingLocation, UUID>
 {
-    @Override
-    @Secured("ROLE_ADMIN")
-    <S extends HousingLocation> S saveAndFlush(S entity) ;
-
-    @Override
-    @Secured("ROLE_ADMIN")
-    <S extends HousingLocation> List<S> saveAllAndFlush(Iterable<S> entities) ;
+    @Secured({"ROLE_CLIENT", "ROLE_ADMIN"})
+    @RestResource(path = "cityOrName", rel = "searchByCityOrName")
+    <S extends HousingLocation> Page<S> searchAllByCityContainingIgnoreCaseOrNameContainingIgnoreCase(
+            String city, String name, Pageable pageable
+    ) ;
 
     @Override
     @Secured("ROLE_ADMIN")
