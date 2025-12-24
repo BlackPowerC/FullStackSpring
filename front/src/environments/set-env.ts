4ea2c1f3f92f0360aa0fd928b271afa81371474c
jojo <jordy.fatigba@theopentrade.com>
2025-12-24 11:35:58 +0000

    chore(frontend): génération de l'environnement de prod avec un script et un .env.

---
+++ front/src/environments/set-env.ts
@@ -0,0 +1,33 @@
+function setEnv(): void
+{
+    const fs = require("fs") ;
+    const colors = require("colors") ;
+
+    const targetPath: string = "./src/environments/environment.ts";
+    require("dotenv").config({
+        path: "./src/environments/.env"
+    }) ;
+
+    const envConfigFile = `export const environment = {
+          apiBaseUrl: '${process.env["API_BASE_URL"]}',
+          apiUser: '${process.env["API_USER"]}',
+          apiBaseUrl: '${process.env["API_BASE_URL"]}',
+          production: true,
+        };` ;
+
+    console.log(colors.magenta('The file `environment.ts` will be written with the following content: \n')) ;
+
+    fs.writeFile(targetPath, envConfigFile, (err: unknown): void =>
+    {
+        if (err)
+        {
+            console.error(err) ;
+            throw err;
+        }
+        else {
+            console.log(colors.magenta(`Angular environment.ts file generated correctly at ${targetPath} \n`)) ;
+        }
+    }) ;
+}
+
+setEnv() ;
